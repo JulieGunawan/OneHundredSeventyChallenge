5052904318d6ccafa3b4f7c0c5c36548dd45c27f
JulieGunawan <julie.gunawan@protonmail.com>
2025-03-23 14:38:24 -0600

    fix: findDOMNode is not a function error

--- firstProject/src/components/dropdown/DropDownMenu.tsx
+++ firstProject/src/components/dropdown/DropDownMenu.tsx
@@ -1,37 +1,63 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import ChevronIcon from "../icons/ChevronIcon";
 import CogIcon from "../icons/CogIcon";
 import "./DropDownMenu.scss"
 import {CSSTransition} from 'react-transition-group';
+import ArrowIcon from "../icons/ArrowIcon";
 
 type DropDownItemProps = {
     children: React.ReactNode;
     leftIcon?: React.ReactNode;
     rightIcon?: React.ReactNode;
+    goToMenu?: string
 }
 const DropDownMenu = () =>{
     const [activeMenu, setActiveMenu] = useState('main');
 
+    const nodeRefPrimary = useRef(null);
+    const nodeRefSecondary = useRef(null);
+
     const DropdownItem = (props: DropDownItemProps) =>{
         return (
-            <a href="#" className="menu-item">
+            <a href="#" className="menu-item" onClick={()=>props.goToMenu && setActiveMenu(props.goToMenu)}>
                 {props.leftIcon && <span className="icon-button">{props.leftIcon}</span> }
                 {props.children}
                 {props.rightIcon && <span className="icon-right">{props.rightIcon}</span>}
             </a>
         )
     }
+    console.log("activeMenu", activeMenu)
     return(
-        <div className="dropdown">
+        <div className="dropdown" >
             <CSSTransition 
                 in={activeMenu === 'main'} 
+                nodeRef={nodeRefPrimary}
                 unmountOnExit 
                 timeout={500} 
                 classNames="menu-primary"
             >
-                <div className="menu">
+                <div className="menu" ref={nodeRefPrimary}>
                     <DropdownItem>My Profile</DropdownItem>
-                    <DropdownItem leftIcon={<CogIcon/>} rightIcon={<ChevronIcon/>}>
+                    <DropdownItem 
+                        leftIcon={<CogIcon/>} 
+                        rightIcon={<ChevronIcon/>}
+                        goToMenu="settings"
+                    >
+                        Settings
+                    </DropdownItem>
+                </div>
+            </CSSTransition>
+
+            <CSSTransition
+                in={activeMenu === 'settings'} 
+                nodeRef={nodeRefSecondary}
+                unmountOnExit 
+                timeout={500} 
+                classNames="menu-secondary"
+            >
+                 <div className="menu" ref={nodeRefSecondary}>
+                    <DropdownItem leftIcon={<ArrowIcon />} goToMenu="main">Settings</DropdownItem>
+                    <DropdownItem>
                         Settings
                     </DropdownItem>
                 </div>
