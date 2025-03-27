93ddc352bf575f205dab72405fc31343fe23096b
JulieGunawan <julie.gunawan@protonmail.com>
2025-03-26 20:17:50 -0600

    feat: Add some footer

---
+++ firstProject/src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import "./Footer.scss"
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <small>2025 All rights reserved</small>
+    </footer>
+  )
+}
+
+export default Footer;
