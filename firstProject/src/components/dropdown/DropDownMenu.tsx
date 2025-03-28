ef1e333ecc8e9d104bd272c3b8bd5861c3064e7f
JulieGunawan <julie.gunawan@protonmail.com>
2025-03-28 14:20:55 -0600

    feat: Added Google Inter font

--- firstProject/src/components/dropdown/DropDownMenu.tsx
+++ firstProject/src/components/dropdown/DropDownMenu.tsx
@@ -12,78 +12,78 @@ type DropDownItemProps = {
     goToMenu?: string
 }
 const DropDownMenu = () =>{
-    const [activeMenu, setActiveMenu] = useState('main');
-    const nodeRefPrimary = useRef(null);
-    const nodeRefSecondary = useRef(null);
+  const [activeMenu, setActiveMenu] = useState('main');
+  const nodeRefPrimary = useRef(null);
+  const nodeRefSecondary = useRef(null);
 
-    const DropdownItem = (props: DropDownItemProps) =>{
-        return (
-            <a href="#" className="menu-item" onClick={()=>props.goToMenu && setActiveMenu(props.goToMenu)}>
-                {props.leftIcon && <span className="icon-button">{props.leftIcon}</span> }
-                {props.children}
-                {props.rightIcon && <span className="icon-right">{props.rightIcon}</span>}
-            </a>
-        )
-    }
-    
-    return(
-        <div className="dropdown"  >
-            <CSSTransition 
-                in={activeMenu === 'main'} 
-                nodeRef={nodeRefPrimary}
-                unmountOnExit 
-                timeout={500} 
-                classNames="menu-primary"
-            >
-                <div className="menu" ref={nodeRefPrimary}>
-                    <DropdownItem>My Profile</DropdownItem>
-                    <DropdownItem 
-                        leftIcon={<CogIcon/>} 
-                        rightIcon={<ChevronIcon/>}
-                        goToMenu="settings"
-                    >
-                        Settings
-                    </DropdownItem>
-                </div>
-            </CSSTransition>
-
-            <CSSTransition
-                in={activeMenu === 'settings'} 
-                nodeRef={nodeRefSecondary}
-                unmountOnExit 
-                timeout={500} 
-                classNames="menu-secondary"
-            >
-                 <div className="menu" ref={nodeRefSecondary}>
-                    <DropdownItem leftIcon={<ArrowIcon />} goToMenu="main">Settings</DropdownItem>
-                    <DropdownItem>
-                        Settings
-                    </DropdownItem>
-                    <DropdownItem>
-                        Settings
-                    </DropdownItem>
-                    <DropdownItem>
-                        Settings
-                    </DropdownItem>
-                    <DropdownItem>
-                        Settings
-                    </DropdownItem>
-                    <DropdownItem>
-                        Settings
-                    </DropdownItem>
-                    <DropdownItem>
-                        Settings
-                    </DropdownItem>
-                    <DropdownItem>
-                        Settings
-                    </DropdownItem>
-                    <DropdownItem>
-                        Settings
-                    </DropdownItem>
-                </div>
-            </CSSTransition>
-        </div>
+  const DropdownItem = (props: DropDownItemProps) =>{
+    return (
+      <a href="#" className="menu-item" onClick={()=>props.goToMenu && setActiveMenu(props.goToMenu)}>
+        {props.leftIcon && <span className="icon-button">{props.leftIcon}</span> }
+        {props.children}
+        {props.rightIcon && <span className="icon-right">{props.rightIcon}</span>}
+      </a>
     )
+  }
+    
+  return(
+    <div className="dropdown"  >
+      <CSSTransition 
+        in={activeMenu === 'main'} 
+        nodeRef={nodeRefPrimary}
+        unmountOnExit 
+        timeout={500} 
+        classNames="menu-primary"
+      >
+        <div className="menu" ref={nodeRefPrimary}>
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem 
+            leftIcon={<CogIcon/>} 
+            rightIcon={<ChevronIcon/>}
+            goToMenu="settings"
+          >
+                        Settings
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === 'settings'} 
+        nodeRef={nodeRefSecondary}
+        unmountOnExit 
+        timeout={500} 
+        classNames="menu-secondary"
+      >
+        <div className="menu" ref={nodeRefSecondary}>
+          <DropdownItem leftIcon={<ArrowIcon />} goToMenu="main">Settings</DropdownItem>
+          <DropdownItem>
+                        Settings
+          </DropdownItem>
+          <DropdownItem>
+                        Settings
+          </DropdownItem>
+          <DropdownItem>
+                        Settings
+          </DropdownItem>
+          <DropdownItem>
+                        Settings
+          </DropdownItem>
+          <DropdownItem>
+                        Settings
+          </DropdownItem>
+          <DropdownItem>
+                        Settings
+          </DropdownItem>
+          <DropdownItem>
+                        Settings
+          </DropdownItem>
+          <DropdownItem>
+                        Settings
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
+  )
 }
 
 export default DropDownMenu;
